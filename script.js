c67149c6457ffef5a65e677a6a831679a0bfba71
HimanshuKumar <himanshu748808@gmail.com>
2025-02-15 06:29:29 +0530

    added search button in consultant

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ document.querySelector(".login-btn").addEventListener("click", function () {
     console.log("Login clicked");
     window.open('login.html', '_blank');
   });
+
+
+  document.querySelector(".hero-btn-primary").addEventListener("click", function () {
+    console.log("Sign Up clicked");
+    window.location.href = 'journal-js-master/index.html'; // You can change this to any other page
+  });
+
+  document.querySelector(".hero-btn-secondary").addEventListener("click", function () {
+    console.log("Sign Up clicked");
+    window.location.href = 'https://drive.google.com/file/d/1ptE8VIxFcgKfNkxpefJ1LztHmyQ9ETKJ/view?usp=sharing'; // You can change this to any other page
+  });
